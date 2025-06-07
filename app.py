a755a1a12f194347e61abc7dcfd40251fef26e48
Xurr1C4n3-byte <xurr1C4n3@gmail.com>
2025-06-07 13:19:09 +0630

    Update app.py

--- app.py
+++ app.py
@@ -3,13 +3,12 @@ import os
 
 app = Flask(__name__)
 
-WEBHOOK_URL = https://webhook.site/8026044f-5d84-4804-ac4c-eaa0796bbd35
+WEBHOOK_URL = "https://webhook.site/8026044f-5d84-4804-ac4c-eaa0796bbd35"
 
-@app.route(/)
+@app.route("/")
 def index():
     return redirect(WEBHOOK_URL, code=302)
 
-if __name__ == __main__:
-    port = int(os.environ.get(PORT, 8080))  # Railway uses dynamic ports
-    app.run(host=0.0.0.0, port=port)
-
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
