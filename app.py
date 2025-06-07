7f5278d35cedb41fa4b770d6d04d392aa0242783
Xurr1C4n3-byte <xurr1C4n3@gmail.com>
2025-06-07 13:22:56 +0630

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, redirect
+from flask import Flask, Response
+import requests
 import os
 
 app = Flask(__name__)
@@ -7,8 +8,10 @@ WEBHOOK_URL = "https://webhook.site/8026044f-5d84-4804-ac4c-eaa0796bbd35"
 
 @app.route("/")
 def index():
-    return redirect(WEBHOOK_URL, code=302)
+    resp = requests.get(WEBHOOK_URL)
+    return Response(resp.content, status=resp.status_code, content_type=resp.headers['Content-Type'])
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
+
